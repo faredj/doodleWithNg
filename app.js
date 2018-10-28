e338c98b5e3fa7de66029dd812b0d812029c713f
f-hammache <faredj.hammache@etu.u-pec.fr>
2018-10-28 16:36:57 +0100

    delete book

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'public/dist/angular6')));
 app.use('/', express.static(path.join(__dirname, 'public/dist/angular6')));
-app.use('/book', book);
 app.use('/user', user);
 
 // catch 404 and forward to error handler
